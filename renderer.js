e0dc44c855b9fff08a6d32474cdb9cf63e9fe4e5
Andrew <andyphommarata@gmail.com>
2025-11-13 22:53:55 +1100

    fix landing-page navigation button to main-ui page

--- renderer.js
+++ renderer.js
@@ -2,6 +2,10 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
+document.getElementById('nav-main').addEventListener('click', () => {
+  window.location.href = 'app/pages/main.html'; // Navigate to main.html page
+});
+
 // Async function to demonstrate IPC communication between renderer and main process through 'ping' channel we defined in main.js + renderer.js
 const func = async () => {
   const response = await window.versions.ping()
